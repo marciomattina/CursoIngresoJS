c4e2d7e682ab58a381bd8b9a4886f0d3f9be2c05
marciomattina <31032622+marciomattina@users.noreply.github.com>
2018-02-06 22:16:22 -0300

    Add files via upload

---
+++ Ejercicios InstruccionIf/jsInstruccionIF (7).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad;
+edad = document.getElementById('edad').value;
+estadoCivil = document.getElementById('estadoCivil').value;
+if (edad<18 && estadoCivil!="Soltero")
+	{
+		alert("es muy pequeño para NO ser soltero");
+	}
+	
+
+
+}//FIN DE LA FUNCIÓN
